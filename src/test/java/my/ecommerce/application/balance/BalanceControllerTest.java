084311c0be8f0d88aab1eee2ef607830ddb9954a
han <bjh4135@gmail.com>
2024-04-02 23:12:34 +0900

    Update: BalanceControllerTest / add argument captor to test exactly what controller do: mapping request dto and serve parameters to service methods

--- src/test/java/my/ecommerce/application/balance/BalanceControllerTest.java
+++ src/test/java/my/ecommerce/application/balance/BalanceControllerTest.java
@@ -1,10 +1,13 @@
 package my.ecommerce.application.balance;
 
-import my.ecommerce.Mocks;
+import my.ecommerce.application.balance.dto.BalanceResponseDto;
+import my.ecommerce.domain.balance.Balance;
 import org.json.JSONObject;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
@@ -12,6 +15,8 @@ import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -20,6 +25,10 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @WebMvcTest(BalanceController.class)
 public class BalanceControllerTest {
+    @Captor
+    ArgumentCaptor<Long> userIdCaptor;
+    @Captor
+    ArgumentCaptor<Long> amountCaptor;
     private MockMvc mockMvc;
     @MockBean
     private BalanceService balanceService;
@@ -35,15 +44,16 @@ public class BalanceControllerTest {
     @WithMockUser
     public void findBalance_success() throws Exception {
         long userId = 1;
-        long amount = 1000;
-        when(balanceService.getOne(userId))
-                .thenReturn(Mocks.mockBalanceResponseDto(amount));
+        when(balanceService.getOne(userId)).thenReturn(mockResponse());
 
         mockMvc
                 .perform(get("/api/v1/balance").header("Authorization", userId))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.code").value("OK"))
-                .andExpect(jsonPath("$.data.amount").value(amount));
+                .andExpect(jsonPath("$.data").exists());
+
+        verify(balanceService).getOne(userIdCaptor.capture());
+        assertEquals(userId, userIdCaptor.getValue());
     }
 
     @Test
@@ -52,8 +62,8 @@ public class BalanceControllerTest {
     public void charge_success() throws Exception {
         long userId = 1;
         long amount = 1000;
-        when(balanceService.charge(userId, amount))
-                .thenReturn(Mocks.mockBalanceResponseDto(amount));
+
+        when(balanceService.charge(userId, amount)).thenReturn(mockResponse());
 
         JSONObject body = new JSONObject();
         body.put("amount", amount);
@@ -67,6 +77,14 @@ public class BalanceControllerTest {
                 )
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.code").value("OK"))
-                .andExpect(jsonPath("$.data.amount").value(amount));
+                .andExpect(jsonPath("$.data").exists());
+
+        verify(balanceService).charge(userIdCaptor.capture(), amountCaptor.capture());
+        assertEquals(userId, userIdCaptor.getValue());
+        assertEquals(amount, amountCaptor.getValue());
+    }
+
+    private BalanceResponseDto mockResponse() {
+        return new BalanceResponseDto(new Balance());
     }
 }
