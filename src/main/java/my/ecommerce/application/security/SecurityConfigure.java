b2941755b97e56182ef52ee19f027ca2169a4478
han <bjh4135@gmail.com>
2024-04-11 17:41:20 +0900

    Refactor: Security / configuration 코드 공식 문서 제안대로 수정, default security automation off

--- src/main/java/my/ecommerce/application/security/SecurityConfigure.java
+++ src/main/java/my/ecommerce/application/security/SecurityConfigure.java
@@ -3,15 +3,22 @@ package my.ecommerce.application.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @EnableWebSecurity
 public class SecurityConfigure {
-    @Bean
-    public SecurityFilterChain chainFilter(HttpSecurity http) throws Exception {
-        return http
-                .addFilter(new AuthenticationFilter())
-                .sessionManagement(new SessionConfigureCustomizer())
-                .build();
-    }
+	@Bean
+	public SecurityFilterChain chainFilter(HttpSecurity http) throws Exception {
+		return http
+			.csrf(AbstractHttpConfigurer::disable)
+			.formLogin(AbstractHttpConfigurer::disable)
+			.authorizeHttpRequests(request -> request
+				.requestMatchers(AntPathRequestMatcher.antMatcher("/**")).permitAll())
+			.sessionManagement(management -> management.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+			.addFilter(new AuthenticationFilter())
+			.build();
+	}
 }
