089c5dda1119cea950d7da694d9bd66a2fe2a546
han <bjh4135@gmail.com>
2024-04-15 15:15:11 +0900

    Refactor: Architecture 변경 / OrderApplication 은 주문 기능에 필요한 서비스를 호출합니다.

--- src/main/java/my/ecommerce/application/OrderApplication.java
+++ src/main/java/my/ecommerce/application/OrderApplication.java
@@ -1,13 +1,39 @@
 package my.ecommerce.application;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import my.ecommerce.domain.balance.BalanceService;
 import my.ecommerce.domain.order.Order;
+import my.ecommerce.domain.order.OrderService;
 import my.ecommerce.domain.order.dto.CreateOrderDto;
+import my.ecommerce.domain.product.Product;
+import my.ecommerce.domain.product.ProductService;
 
 @Component
 public class OrderApplication {
-	public Order order(CreateOrderDto createOrderDto) {
-		return Order.builder().build();
+	private final OrderService orderService;
+	private final ProductService productService;
+	private final BalanceService balanceService;
+
+	@Autowired
+	public OrderApplication(OrderService orderService, ProductService productService, BalanceService balanceService) {
+		this.orderService = orderService;
+		this.productService = productService;
+		this.balanceService = balanceService;
+	}
+
+	public Order run(CreateOrderDto createOrderDto) {
+		Order order = Order.newOrder(createOrderDto.getUserId(), createOrderDto.getTotalPrice());
+		createOrderDto.getItems().forEach(item -> {
+			Product product = productService.getAvailableProduct(item.getProductId(), item.getQuantity());
+			order.addOrderItem(product, item.getQuantity(), item.getCurrentPrice());
+		});
+
+		balanceService.use(order.getUserId(), order.getTotalPrice());
+
+		order.getItems().forEach(item -> productService.sell(item.getProduct(), item.getQuantity()));
+
+		return orderService.create(order);
 	}
 }
---
+++ src/test/java/my/ecommerce/application/OrderApplicationTest.java
@@ -0,0 +1,127 @@
+package my.ecommerce.application;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+import static org.mockito.MockitoAnnotations.*;
+
+import java.util.List;
+import java.util.UUID;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.Mock;
+
+import my.ecommerce.domain.balance.BalanceService;
+import my.ecommerce.domain.order.Order;
+import my.ecommerce.domain.order.OrderService;
+import my.ecommerce.domain.order.dto.CreateOrderDto;
+import my.ecommerce.domain.order.dto.CreateOrderItemDto;
+import my.ecommerce.domain.product.Product;
+import my.ecommerce.domain.product.ProductService;
+import my.ecommerce.utils.UUIDGenerator;
+
+public class OrderApplicationTest {
+	@Captor
+	ArgumentCaptor<UUID> productIdCaptor;
+	@Captor
+	ArgumentCaptor<Long> quantityCaptor;
+	@Captor
+	ArgumentCaptor<UUID> userIdCaptor;
+	@Captor
+	ArgumentCaptor<Long> totalPriceCaptor;
+	@Captor
+	ArgumentCaptor<Product> productCaptor;
+	@Captor
+	ArgumentCaptor<Long> sellCountCaptor;
+	@Captor
+	ArgumentCaptor<Order> orderCaptor;
+
+	@Mock
+	OrderService orderService;
+	@Mock
+	ProductService productService;
+	@Mock
+	BalanceService balanceService;
+
+	private OrderApplication orderApplication;
+
+	@BeforeEach()
+	void setOrderApplication() {
+		openMocks(this);
+		this.orderApplication = new OrderApplication(orderService, productService, balanceService);
+	}
+
+	@Test
+	@DisplayName("올바른 Dto 로 각 서비스를 호출하여 주문 성공")
+	void success_order() {
+		// Given
+		Product product = prepareProduct();
+		CreateOrderDto orderDto = prepareCreateOrderDto(product);
+		CreateOrderItemDto orderItemDto = orderDto.getItems().get(0);
+
+		mockProductServiceGetProduct(orderItemDto, product);
+		mockBalanceService();
+		mockProductServiceSellProduct(product, orderItemDto);
+		mockOrderService(orderDto);
+
+		// When
+		orderApplication.run(orderDto);
+
+		// Then
+		verify(productService).getAvailableProduct(productIdCaptor.capture(), quantityCaptor.capture());
+		assertEquals(product.getId(), productIdCaptor.getValue());
+		assertEquals(orderItemDto.getQuantity(), quantityCaptor.getValue());
+
+		verify(balanceService).use(userIdCaptor.capture(), totalPriceCaptor.capture());
+		assertEquals(orderDto.getUserId(), userIdCaptor.getValue());
+		assertEquals(orderDto.getTotalPrice(), totalPriceCaptor.getValue());
+
+		verify(productService).sell(productCaptor.capture(), sellCountCaptor.capture());
+		assertEquals(product, productCaptor.getValue());
+		assertEquals(orderItemDto.getQuantity(), sellCountCaptor.getValue());
+
+		verify(orderService).create(orderCaptor.capture());
+		Order order = orderCaptor.getValue();
+		assertEquals(orderDto.getUserId(), order.getUserId());
+		assertEquals(orderDto.getItems().size(), order.getItems().size());
+		assertNull(order.getId());
+	}
+
+	private Product prepareProduct() {
+		return Product.builder().id(UUIDGenerator.generate()).build();
+	}
+
+	private CreateOrderDto prepareCreateOrderDto(Product product) {
+		List<CreateOrderItemDto> itemsDto = List.of(CreateOrderItemDto.builder()
+			.productId(product.getId())
+			.quantity(1)
+			.build());
+
+		return CreateOrderDto.builder()
+			.userId(UUIDGenerator.generate())
+			.items(itemsDto)
+			.totalPrice(1000)
+			.build();
+	}
+
+	private void mockProductServiceGetProduct(CreateOrderItemDto orderItemDto, Product product) {
+		when(productService.getAvailableProduct(orderItemDto.getProductId(), orderItemDto.getQuantity())).thenReturn(
+			product);
+	}
+
+	private void mockProductServiceSellProduct(Product product, CreateOrderItemDto orderItemDto) {
+		doNothing().when(productService).sell(product, orderItemDto.getQuantity());
+	}
+
+	private void mockBalanceService() {
+		doNothing().when(balanceService).use(any(UUID.class), anyLong());
+	}
+
+	private void mockOrderService(CreateOrderDto orderDto) {
+		when(orderService.create(any(Order.class))).thenReturn(
+			Order.newOrder(orderDto.getUserId(), orderDto.getTotalPrice()));
+	}
+}
