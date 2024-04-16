2716a6e386bad143f49365011ea4bc8673af01df
han <bjh4135@gmail.com>
2024-04-17 00:07:28 +0900

    Feat: BaseDomain / ID 생성을 비롯한 domain 객체의 persistence 를 관리

--- src/main/java/my/ecommerce/datasource/entity/BaseEntity.java
+++ src/main/java/my/ecommerce/datasource/entity/BaseEntity.java
@@ -5,15 +5,18 @@ import java.util.UUID;
 
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.lang.Nullable;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import my.ecommerce.utils.UUIDGenerator;
 
 @MappedSuperclass
 @Getter
+@NoArgsConstructor
 public abstract class BaseEntity {
 	@Id
 	protected UUID id;
@@ -26,8 +29,8 @@ public abstract class BaseEntity {
 	@Column(nullable = false, name = "updated_at")
 	protected LocalDateTime updatedAt;
 
-	public BaseEntity() {
-		this.id = generatePK();
+	public BaseEntity(@Nullable UUID id) {
+		this.id = id == null ? generatePK() : id;
 	}
 
 	private UUID generatePK() {
