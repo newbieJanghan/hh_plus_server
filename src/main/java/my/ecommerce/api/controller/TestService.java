3706ec558c23c693b1c43cb94fa72a1402001aa5
han <bjh4135@gmail.com>
2024-04-19 17:04:39 +0900

    Chore: Delete unused codes

--- src/main/java/my/ecommerce/api/controller/TestService.java
+++
@@ -1,9 +0,0 @@
-package my.ecommerce.api.controller;
-
-import org.springframework.stereotype.Service;
-
-@Service("testService")
-public class TestService {
-	public void exception() throws Exception {
-	}
-}
