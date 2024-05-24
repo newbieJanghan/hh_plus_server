04f4429db7a9559453dccdfced48b73f13e4221f
newbiejanghan <bjh4135@gmail.com>
2024-05-24 20:54:11 +0900

    Chore: 불필요한 코드 삭제

--- src/main/java/my/ecommerce/ECommerceApplication.java
+++ src/main/java/my/ecommerce/ECommerceApplication.java
@@ -1,9 +1,5 @@
 package my.ecommerce;
 
-import java.time.LocalDateTime;
-import java.time.ZoneOffset;
-import java.util.concurrent.ThreadLocalRandom;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -11,13 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ECommerceApplication {
 
 	public static void main(String[] args) {
-		LocalDateTime from = LocalDateTime.of(2024, 3, 1, 0, 0, 0);
-		LocalDateTime to = LocalDateTime.of(2024, 9, 1, 0, 0, 0);
-		LocalDateTime randomDate = LocalDateTime.ofEpochSecond(
-			ThreadLocalRandom.current().nextLong(from.toEpochSecond(ZoneOffset.UTC), to.toEpochSecond(ZoneOffset.UTC)),
-			0, ZoneOffset.UTC);
-		System.out.println(randomDate);
-
 		SpringApplication.run(ECommerceApplication.class, args);
 	}
 
